53df8033ea2624ea2687c6b6fb9a1db758d7baf9
Kimuri <kimvianney6@gmail.com>
2020-06-08 16:45:16 +0300

    Setting up the controls and the environment

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,56 @@
 document.addEventListener("DOMContentLoaded", () =>{
-
-
-
-
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const ScoreDisplay = document.getElementById('score');
+    const StartBtn = document.querySelector('#start-button');
+    
+    const width = 10;
 
     
-} )
+    // Creating the Tetrominoes
+    const lTetromino  = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]       
+    ]
+
+    const zTetromino = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2+1, width*2],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2+1, width*2]
+
+    ]
+
+    const tTetromino = [
+        [1, width, width+1, width+2],
+        [width+2, 1, width+1, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [width, 1, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width+1, width+2, width+3, width+4],
+        [1, width+1, width*2+1, width*3+1],
+        [width+1, width+2, width+3, width+4]
+
+    ]
+
+    
+
+
+
+
+
+
+} );
