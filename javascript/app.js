8f9fd4a613d7b829099fc1b00cd2836ae5e4a07a
Kimuri <kimvianney6@gmail.com>
2020-06-08 17:26:12 +0300

    New Progress

--- javascript/app.js
+++ javascript/app.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () =>{
         [width, width+1, width+2, width*2+2],
         [1, width+1, width*2+1, width*2],
         [width, width*2, width*2+1, width*2+2]       
-    ]
+    ];
 
     const zTetromino = [
         [0, width, width+1, width*2+1],
@@ -21,14 +21,14 @@ document.addEventListener("DOMContentLoaded", () =>{
         [0, width, width+1, width*2+1],
         [width+1, width+2, width*2+1, width*2]
 
-    ]
+    ];
 
     const tTetromino = [
         [1, width, width+1, width+2],
         [width+2, 1, width+1, width*2+1],
         [width, width+1, width+2, width*2+1],
         [width, 1, width+1, width*2+1]
-    ]
+    ];
 
     const oTetromino = [
         [0, 1, width, width+1],
@@ -36,16 +36,38 @@ document.addEventListener("DOMContentLoaded", () =>{
         [0, 1, width, width+1],
         [0, 1, width, width+1],
 
-    ]
+    ];
 
     const iTetromino = [
         [1, width+1, width*2+1, width*3+1],
-        [width+1, width+2, width+3, width+4],
+        [width, width+1, width+2, width+3],
         [1, width+1, width*2+1, width*3+1],
-        [width+1, width+2, width+3, width+4]
+        [width, width+1, width+2, width+3]
 
-    ]
+    ];
 
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+    
+
+    // To draw the first rotation of the lTetromino
+    let currentPosition = 4;
+    let currentRotation = 0;
+
+    //To access a random tetromino 
+    let randomTetro =Math.floor(Math.random() * theTetrominoes.length);
+
+    let current = theTetrominoes[randomTetro][currentRotation];
+
+    //To draw the tetrominoes on the grid
+    function draw(){
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add("tetromino");
+        })
+
+
+    }
+
+    //To undraw the tetrominoes from the screen
     
 
 
