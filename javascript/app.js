75790d8c9ef0efb57f9710f267a8973f903a0a57
Kimuri <kimvianney6@gmail.com>
2020-06-12 13:09:17 +0300

    Programming the controls

--- javascript/app.js
+++ javascript/app.js
@@ -1,65 +1,65 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector('.grid');
     let squares = Array.from(document.querySelectorAll('.grid div'));
     const ScoreDisplay = document.getElementById('score');
     const StartBtn = document.querySelector('#start-button');
-    
+
     const width = 10;
 
-    
+
     // Creating the Tetrominoes
-    const lTetromino  = [
-        [1, width+1, width*2+1, 2],
-        [width, width+1, width+2, width*2+2],
-        [1, width+1, width*2+1, width*2],
-        [width, width*2, width*2+1, width*2+2]       
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
     ];
 
     const zTetromino = [
-        [0, width, width+1, width*2+1],
-        [width+1, width+2, width*2+1, width*2],
-        [0, width, width+1, width*2+1],
-        [width+1, width+2, width*2+1, width*2]
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2 + 1, width * 2],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2 + 1, width * 2]
 
     ];
 
     const tTetromino = [
-        [1, width, width+1, width+2],
-        [width+2, 1, width+1, width*2+1],
-        [width, width+1, width+2, width*2+1],
-        [width, 1, width+1, width*2+1]
+        [1, width, width + 1, width + 2],
+        [width + 2, 1, width + 1, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [width, 1, width + 1, width * 2 + 1]
     ];
 
     const oTetromino = [
-        [0, 1, width, width+1],
-        [0, 1, width, width+1],
-        [0, 1, width, width+1],
-        [0, 1, width, width+1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
 
     ];
 
     const iTetromino = [
-        [1, width+1, width*2+1, width*3+1],
-        [width, width+1, width+2, width+3],
-        [1, width+1, width*2+1, width*3+1],
-        [width, width+1, width+2, width+3]
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
 
     ];
 
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
-    
+
 
     // To draw the first rotation of the lTetromino
     let currentPosition = 4;
     let currentRotation = 0;
 
     //To access a random tetromino 
-    let randomTetro =Math.floor(Math.random() * theTetrominoes.length);
+    let randomTetro = Math.floor(Math.random() * theTetrominoes.length);
 
     let current = theTetrominoes[randomTetro][currentRotation];
 
     //To draw the tetrominoes on the grid
-    function draw(){
+    function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add("tetromino");
         })
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     }
 
     //To undraw the tetrominoes from the screen
-    function undraw(){
+    function undraw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino');
 
@@ -79,16 +79,32 @@ document.addEventListener("DOMContentLoaded", () =>{
 
     let timerID = setInterval(moveDown, 1000);
 
-    function moveDown(){
+    function moveDown() {
         undraw();
         currentPosition += width;
         draw();
         freeze();
     }
 
+
+    //Programming the controls
+    document.addEventListener('keyup', controls);
+
+    function controls(e) {
+        if (e.keyCode === 37) {
+            moveLeft();
+        } else if (e.keyCode === 38) {
+            rotate()
+        } else if (e.keyCode === 39) {
+            moveRight();
+        } else if (e.keyCode === 40) {
+            moveDown();
+        }
+    }
+
     //Function to prevent the tetrominoes from dropping from the screen
-    function freeze(){
-        if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+    function freeze() {
+        if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
             current.forEach(index => {
                 squares[currentPosition + index].classList.add('taken');
             })
@@ -101,9 +117,54 @@ document.addEventListener("DOMContentLoaded", () =>{
         }
     }
 
+    //Function to move the tetromino towards the left edge
+    function moveLeft() {
+        undraw();
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+
+        if (!isAtLeftEdge) {
+            currentPosition -= 1;
+
+        }
+
+        if (current.some(squares[currentPosition + index]).classList.contains('taken')) {
+            currentPosition += 1;
+        }
+
+        draw();
+    }
+
+    // Function to allow the tetromino to move towards the right edge
+
+    function moveRight(){
+        undraw();
+
+        const isAtRightEdge = current.some(index => (currentPosition + index)% width === width -1);
+
+        if (!isAtRightEdge) {
+            currentPosition += 1;
+        }
+        if(current.some(index => squares[currentPosition + index ].classList.contains('taken'))){
+            current -=1;
+        }
+        draw();
+    }
+
+    // Function to rotate the tetromino 
+    function rotate(){
+        undraw();
+        currentRotation++;
+        if (currentRotation === current.length) {
+            currentRotation = 0;
+        }
+        current = theTetrominoes[randomTetro][currentRotation];
+        draw();
+
+    }
 
 
-    
+
+   
 
 
 
@@ -111,4 +172,5 @@ document.addEventListener("DOMContentLoaded", () =>{
 
 
 
-} );
+
+});
